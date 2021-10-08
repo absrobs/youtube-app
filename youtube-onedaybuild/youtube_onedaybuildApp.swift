dc6633e47f851534ba1efc48a45e02bf727ef1e2
absrobs <abdi.roble@outlook.com>
2021-10-08 17:16:57 +0100

    initial xcode project creation

---
+++ youtube-onedaybuild/youtube_onedaybuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  youtube_onedaybuildApp.swift
+//  youtube-onedaybuild
+//
+//  Created by A Roble on 08/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct youtube_onedaybuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
